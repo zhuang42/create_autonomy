6d8929fa96f30be50ae416b54bbac1e1054a4fe8
Steven <steven.desvars@gmail.com>
2019-12-20 15:59:05 -0300

    Addressed most of the comments

--- ca_gazebo/include/ca_gazebo/cliff_msg_publisher.h
+++ ca_gazebo/include/ca_gazebo/cliff_msg_publisher.h
@@ -1,63 +1,61 @@
-    #ifndef GAZEBO_CLIFF_MSG_PUBLISHER_HH
-    #define GAZEBO_CLIFF_MSG_PUBLISHER_HH
-    
-    #include <ros/ros.h>
-    
-    #include <gazebo/gazebo.hh>
-    #include <gazebo/physics/physics.hh>
-    #include <gazebo/common/common.hh>
-    #include <gazebo/common/Plugin.hh>
-    #include <gazebo/common/Events.hh>
+#ifndef GAZEBO_CLIFF_MSG_PUBLISHER_HH
+#define GAZEBO_CLIFF_MSG_PUBLISHER_HH
 
-    #include <mutex>
+#include <ros/ros.h>
 
-    #include "std_msgs/Bool.h"
-    #include "ca_msgs/Cliff.h"
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/common/Events.hh>
 
-    namespace gazebo
-    {
-        /// \brief A Cliff Msg publisher
-        class GazeboCliffMsgPublisher : public ModelPlugin
-    {
-        public:
+#include <mutex>
+#include <string>
 
-        /// \brief Load the plugin
-        /// \param take in SDF root element
-        void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf); 
+#include "std_msgs/Bool.h"
+#include "ca_msgs/Cliff.h"
 
-        /// \brief Callback to the cliff sensors
-        /// \param take pointer to the message
-        void SideLeftCliffCallback(const std_msgs::Bool::ConstPtr& data); 
-        void SideRightCliffCallback(const std_msgs::Bool::ConstPtr& data);
-        void FrontLeftCliffCallback(const std_msgs::Bool::ConstPtr& data);
-        void FrontRightCliffCallback(const std_msgs::Bool::ConstPtr& data);
+namespace gazebo
+{
+/// \brief A Cliff Msg publisher
+class GazeboCliffMsgPublisher : public ModelPlugin
+{
+public:
 
-        /// \brief Update the controller
-        void OnUpdate();
+  /// \brief Load the plugin
+  /// \param take in SDF root element
+  void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
 
-        private:
+  /// \brief Callback to the cliff sensors
+  /// \param take pointer to the message
+  void SideLeftCliffCallback(const std_msgs::Bool::ConstPtr &data);
+  void SideRightCliffCallback(const std_msgs::Bool::ConstPtr &data);
+  void FrontLeftCliffCallback(const std_msgs::Bool::ConstPtr &data);
+  void FrontRightCliffCallback(const std_msgs::Bool::ConstPtr &data);
 
-        /// Pointer to the model
-        physics::ModelPtr model;
+  /// \brief Update the controller
+  void OnUpdate();
 
-        /// Pointer to the update event connection
-        private: event::ConnectionPtr updateConnection;
+private:
+  /// Pointer to the model
+  physics::ModelPtr model;
 
-        /// Initialize ROS variables
-        ros::NodeHandle nh_;
-        ros::Publisher publisher_;
-        ros::Subscriber side_left_,side_right_, front_left_, front_right_;
-        ros::Time prev_update_time_;
-        event::ConnectionPtr updateConnection_;
-        
-        /// Auxiliar variables
-        char robot_number_;
-        int seq_counter_;
-        std::string frame_name_;
-        bool get_side_left_, get_side_right_, get_front_left_, get_front_right_;
-        double update_period_;
-        std::mutex side_left_mutex, side_right_mutex, front_left_mutex, front_right_mutex;
-      }; // GazeboCliffMsgPublisher
-    } // gazebo
-    #endif // GAZEBO_CLIFF_MSG_PUBLISHER_HH
-    
+  /// Pointer to the update event connection
+  event::ConnectionPtr update_connection_;
+
+  /// Initialize ROS variables
+  ros::NodeHandle nh_;
+  ros::Publisher publisher_;
+  ros::Subscriber side_left_sub_, side_right_sub_, front_left_sub_, front_right_sub_;
+  ros::Time prev_update_time_;
+
+  /// Auxiliar variables
+  ca_msgs::Cliff msg_;
+  std::string robot_name_;
+  std::string frame_name_;
+  bool get_side_left_, get_side_right_, get_front_left_, get_front_right_;
+  double update_period_;
+  std::mutex side_left_mutex, side_right_mutex, front_left_mutex, front_right_mutex;
+}; // GazeboCliffMsgPublisher
+} // namespace gazebo
+#endif // GAZEBO_CLIFF_MSG_PUBLISHER_HH
--- ca_gazebo/src/cliff_msg_publisher.cpp
+++ ca_gazebo/src/cliff_msg_publisher.cpp
@@ -2,119 +2,101 @@
 
 namespace gazebo
 {
-  // Register this plugin with the simulator
-  GZ_REGISTER_MODEL_PLUGIN(GazeboCliffMsgPublisher)
+// Register this plugin with the simulator
+GZ_REGISTER_MODEL_PLUGIN(GazeboCliffMsgPublisher)
 
-  void GazeboCliffMsgPublisher::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+void GazeboCliffMsgPublisher::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+{
+  this->update_period_ = 1.0 / (_sdf->Get<double>("updateRate"));
+  // Make sure the ROS node for Gazebo has already been initialized
+  if (!ros::isInitialized())
   {
-    this->update_period_ = 1.0/(_sdf->Get<double>("updateRate"));
-    // Make sure the ROS node for Gazebo has already been initialized
-    if (!ros::isInitialized())
-    {
-      ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
-        << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
-      return;
-    }
-    if (!ros::isInitialized()) 
-    {
-      int argc = 0;
-      char** argv = NULL;
-      ros::init(argc, argv, "cliff_msg_publisher");
-    }
-    // Initialize the ros variables and gazebo variables
-    this->robot_number_ =_sdf->Get<char>("robotNumber");
-    if (!_sdf->HasElement("frameName"))
-    {
-        ROS_INFO("cliff plugin missing <frameName>, defaults to world");
-        this->frame_name_ = "base_footprint";
-    }
-    else
-        this->frame_name_ = _sdf->Get<std::string>("frameName");
-
-    std::ostringstream oss;
-    oss << "create" << this->robot_number_ << "/side_left_cliff_sensor/raw/";
-    std::string topic_name = oss.str();
-    this->side_left_ = this->nh_.subscribe(topic_name, 1, &GazeboCliffMsgPublisher::SideLeftCliffCallback, this);
-    oss.str("");
-    oss << "create" << this->robot_number_ << "/side_right_cliff_sensor/raw/";
-    topic_name = oss.str();
-    this->side_right_ = this->nh_.subscribe(topic_name, 1, &GazeboCliffMsgPublisher::SideRightCliffCallback, this);
-    oss.str("");
-    oss << "create" << this->robot_number_ << "/front_left_cliff_sensor/raw/";
-    topic_name = oss.str();
-    this->front_left_ = this->nh_.subscribe(topic_name, 1, &GazeboCliffMsgPublisher::FrontLeftCliffCallback, this);
-    oss.str("");
-    oss << "create" << this->robot_number_ << "/front_right_cliff_sensor/raw/";
-    topic_name = oss.str();
-    this->front_right_ = this->nh_.subscribe(topic_name, 1, &GazeboCliffMsgPublisher::FrontRightCliffCallback, this);
-    oss.str("");                  
-    oss << "create" << this->robot_number_ << "/cliff_msg/";
-    topic_name = oss.str();
-    this->publisher_ = this->nh_.advertise<ca_msgs::Cliff>(topic_name, 1);
-    this->seq_counter_ = 0;
-    this->updateConnection_ = event::Events::ConnectWorldUpdateBegin(
-      std::bind(&GazeboCliffMsgPublisher::OnUpdate, this));    
-    this->prev_update_time_ = ros::Time::now();
+    int argc = 0;
+    char **argv = NULL;
+    ros::init(argc, argv, "cliff_msg_publisher");
   }
-
-  ////////////////////////////////////////////////////////////////////////////////
-  // Update the controller
-  void GazeboCliffMsgPublisher::OnUpdate()
-  //Every update check if one of the sensors is active, if it is, change the output to true, publishes every 1ms
+  // Initialize the ros variables and gazebo variables
+  this->robot_name_ = _sdf->Get<std::string>("robotNumber");
+  if (!_sdf->HasElement("frameName"))
   {
-    if ((ros::Time::now() - this->prev_update_time_) < ros::Duration(this->update_period_)) 
-    {
-      return;
-    }
-    ca_msgs::Cliff msg;
-    this->seq_counter_++;
-    msg.header.frame_id = this->frame_name_;
-    msg.header.seq = this->seq_counter_;
-    msg.header.stamp = ros::Time::now();
-    {
-      const std::lock_guard<std::mutex> lock(side_left_mutex);
-      msg.is_cliff_left = this->get_side_left_;
-    }
-      const std::lock_guard<std::mutex> lock(side_right_mutex);
-      msg.is_cliff_right = this->get_side_right_;   
-    {
-      const std::lock_guard<std::mutex> lock(front_left_mutex);
-      msg.is_cliff_front_left = this->get_front_left_;
-    }
-
-    {
-      const std::lock_guard<std::mutex> lock(front_right_mutex);
-      msg.is_cliff_front_right = this->get_front_right_;
-    }
-    this->publisher_.publish(msg);
-    this->prev_update_time_ = ros::Time::now();
+    ROS_INFO("cliff plugin missing <frameName>, defaults to world");
+    this->frame_name_ = "base_footprint";
   }
+  else
+  {
+    this->frame_name_ = _sdf->Get<std::string>("frameName");
+  }
+  this->msg_.header.frame_id = this->frame_name_;
+  this->side_left_sub_ = this->nh_.subscribe(
+      this->robot_name_ + "/side_left_cliff_sensor/raw/", 1, &GazeboCliffMsgPublisher::SideLeftCliffCallback, this);
+  this->side_right_sub_ = this->nh_.subscribe(
+      this->robot_name_ + "/side_right_cliff_sensor/raw/", 1, &GazeboCliffMsgPublisher::SideRightCliffCallback, this);
+  this->front_left_sub_ = this->nh_.subscribe(
+      this->robot_name_ + "/front_left_cliff_sensor/raw/", 1, &GazeboCliffMsgPublisher::FrontLeftCliffCallback, this);
+  this->front_right_sub_ = this->nh_.subscribe(
+      this->robot_name_ + "/front_right_cliff_sensor/raw/", 1, &GazeboCliffMsgPublisher::FrontRightCliffCallback, this);
+  this->publisher_ = this->nh_.advertise<ca_msgs::Cliff>(this->robot_name_ + "/cliff_msg/", 1);
+  this->update_connection_ = event::Events::ConnectWorldUpdateBegin(
+      std::bind(&GazeboCliffMsgPublisher::OnUpdate, this));
+  this->prev_update_time_ = ros::Time::now();
+}
 
-  void GazeboCliffMsgPublisher::SideLeftCliffCallback(const std_msgs::Bool::ConstPtr& data)
-  // Callback to get the data from the side left cliff sensor
+////////////////////////////////////////////////////////////////////////////////
+// Update the controller
+void GazeboCliffMsgPublisher::OnUpdate()
+//Every update check if one of the sensors is active, if it is, change the output to true, publishes every 1ms
+{
+  if ((ros::Time::now() - this->prev_update_time_) < ros::Duration(this->update_period_))
+  {
+    return;
+  }
+  this->msg_.header.seq++;
+  this->msg_.header.stamp = ros::Time::now();
   {
     const std::lock_guard<std::mutex> lock(side_left_mutex);
-    this->get_side_left_ = data->data;
+    this->msg_.is_cliff_left = this->get_side_left_;
   }
-
-    void GazeboCliffMsgPublisher::SideRightCliffCallback(const std_msgs::Bool::ConstPtr& data)
-  // Callback to get the data from the side right cliff sensor
   {
     const std::lock_guard<std::mutex> lock(side_right_mutex);
-    this->get_side_right_ = data->data;
+    this->msg_.is_cliff_right = this->get_side_right_;
   }
-
-    void GazeboCliffMsgPublisher::FrontLeftCliffCallback(const std_msgs::Bool::ConstPtr& data)
-  // Callback to get the data from the front left cliff sensor
   {
     const std::lock_guard<std::mutex> lock(front_left_mutex);
-    this->get_front_left_ = data->data;
+    this->msg_.is_cliff_front_left = this->get_front_left_;
   }
-
-    void GazeboCliffMsgPublisher::FrontRightCliffCallback(const std_msgs::Bool::ConstPtr& data)
-  // Callback to get the data from the front right sensor
   {
     const std::lock_guard<std::mutex> lock(front_right_mutex);
-    this->get_front_right_ = data->data;
+    this->msg_.is_cliff_front_right = this->get_front_right_;
   }
+  this->publisher_.publish(this->msg_);
+  this->prev_update_time_ = ros::Time::now();
 }
+
+// Callback to get the data from the side left cliff sensor
+void GazeboCliffMsgPublisher::SideLeftCliffCallback(const std_msgs::Bool::ConstPtr &data)
+{
+  const std::lock_guard<std::mutex> lock(side_left_mutex);
+  this->get_side_left_ = data->data;
+}
+
+// Callback to get the data from the side right cliff sensor
+void GazeboCliffMsgPublisher::SideRightCliffCallback(const std_msgs::Bool::ConstPtr &data)
+{
+  const std::lock_guard<std::mutex> lock(side_right_mutex);
+  this->get_side_right_ = data->data;
+}
+
+// Callback to get the data from the front left cliff sensor
+void GazeboCliffMsgPublisher::FrontLeftCliffCallback(const std_msgs::Bool::ConstPtr &data)
+{
+  const std::lock_guard<std::mutex> lock(front_left_mutex);
+  this->get_front_left_ = data->data;
+}
+
+// Callback to get the data from the front right sensor
+void GazeboCliffMsgPublisher::FrontRightCliffCallback(const std_msgs::Bool::ConstPtr &data)
+{
+  const std::lock_guard<std::mutex> lock(front_right_mutex);
+  this->get_front_right_ = data->data;
+}
+} // namespace gazebo
